a11955ad8388a801e225f339a389cf3599724e5b
Balaji434839 <balajikumar899@gmail.com>
2024-08-30 17:28:51 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './app.css'
+import Login from './Login.jsx'
+
+
+const App = () => {
+  return (
+   <>
+   {/* <div className='flex items-center justify-around w-full h-32 bg-yellow-500 '>
+    <h1 className="font-bold underline text-pretty">
+    Classical world!
+  </h1> */}
+  
+  {/* <div className="bg-scroll ..." style="background-image: url()"></div> */}
+  {/* <div className="grow h-14 ...">e2duhdq</div> */}
+    {/* <div className="flex items-center justify-center h-full"></div> */}
+
+  
+  {/* <div className="flex flex-auto"></div> */}
+ 
+
+{/* 
+  <ul className="list-image-none md:list-image-[url(frontend/src/assets/bg.png)]">
+  Classical
+</ul>
+   */}
+   {/* <button 
+         className="px-2 py-2 bg-blue-500 rounded fon-bold">
+    
+
+   <ol className='flex gap-8 px-4 py-2 font-bold text-white rounded'>
+   <li> Home</li>
+    <li>Menu</li>
+    <li>About</li>
+    <li>Service</li>    
+   </ol>
+   </button>
+   <button className='px-4 py-2 font-bold text-white bg-blue-500 rounded'>
+    Subscribe</button>
+  </div> */}
+    <Login/>
+</>
+  )
+}
+
+export default App
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,65 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './Login.css'; 
+
+const Login = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    
+    // <div className="bg.png"> 
+{/* <h1 className ="text-3xl font-bold underline">
+    Hello world!
+  </h1> */}
+       ///* <img src="bg.png"></img> */}
+      {/* <div className="content">
+        <header>Login Form</header>
+        <form action="#">
+          <div className="field"> */}
+            {/* <FontAwesomeIcon icon={faUser} /> */}
+            {/* <input type="text" required placeholder="Email or Phone" />
+          </div>
+          <div className="field space"> */}
+            {/* <FontAwesomeIcon icon={faLock} /> */}
+            {/* <input
+              type={isPasswordVisible ? "text" : "password"}
+              className="pass-key"
+              required
+              placeholder="Password"
+            />
+            <span className="show" onClick={togglePasswordVisibility}>
+              {isPasswordVisible ? "HIDE" : "SHOW"}
+            </span> */}
+          {/* </div>
+          <div className="pass">
+            <a href="#">Forgot Password?</a>
+          </div>
+          <div className="field">
+            <input type="submit" value="LOGIN" />
+          </div>
+        </form>
+        <div className="login">Or login with</div>
+        <div className="links">
+          <div className="facebook"> */}
+            {/* <FontAwesomeIcon icon={faFacebookF} /> */}
+            {/* <span>Facebook</span>
+          </div>
+          <div className="instagram"> */}
+            {/* <FontAwesomeIcon icon={faInstagram} /> */}
+            {/* <span>Instagram</span>
+          </div>
+        </div>
+        <div className="signup">
+          Dont have an account? <a href="#">Signup Now</a>
+           */}
+        // {/* </div> */}
+      // {/* // </div> */}
+    // {/* // </div> */}
+    
+) } 
+
+export default Login;
